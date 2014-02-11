6ba1a08887cc7db373b01d2f20b163c59de40165
Omri Dor <omridor@gmail.com>
2014-02-11 11:29:32 +0200

    User detail page now also displays total number of commits

--- users/views.py
+++ users/views.py
@@ -12,3 +12,9 @@ class UserView(DetailView):
     # TODO: show users according to privacy level
     model = User
     slug_field = 'nick'
+
+    def get_context_data(self, **kwargs):
+        context = super(DetailView, self).get_context_data(**kwargs)
+        commits = self.object.authored_commits
+        context['total_commits'] = len(commits.all())
+        return context
