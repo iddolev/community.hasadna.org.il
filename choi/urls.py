b22fc52b7d0cab5c6b060bb4e906b457bfa621da
Omri Dor <omridor@gmail.com>
2014-02-24 21:05:48 +0200

    Added homepage

--- choi/urls.py
+++ choi/urls.py
@@ -1,11 +1,12 @@
 from django.conf.urls import patterns, include, url
+from django.views.generic import TemplateView
 
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'choi.views.home', name='home'),
+    url(r'^$',  TemplateView.as_view(template_name='home.html'), name='home'),
     url(r'^users/', include('users.urls', namespace='users')),
     url(r'^projects/', include('repos.urls', namespace='repos')),
     url(r'^admin/', include(admin.site.urls)),
